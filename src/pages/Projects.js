2aeb8a2f447a77eb8c7ceb6aad9273f75d736d42
Daniel Rodriguez <MissingRed@github.com>
2021-03-08 00:11:27 -0500

    62%

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -47,12 +47,22 @@ const Projects = () => {
       .then((data) => {
         const array = [];
         data.map((rep) => {
-          const name = rep.name;
-          const constdesc = rep.description;
-          const url = rep.html_url;
-          const fechas = rep.updated_at.slice(0, -10);
-          array.push({ fechas, name, constdesc, url });
-          return true;
+          if (
+            rep.name === "Portfolio-UI-2021" ||
+            rep.name === "MasterPC" ||
+            rep.name === "CodeShop" ||
+            rep.name === "gif-expert-app" ||
+            rep.name === "CodeShopV3" ||
+            rep.name === "Bush-lol" ||
+            rep.name === "PreguntadosUSC-Ionic"
+          ) {
+            const name = rep.name;
+            const constdesc = rep.description;
+            const url = rep.html_url;
+            const fechas = rep.updated_at.slice(0, -10);
+            array.push({ fechas, name, constdesc, url });
+            return true;
+          }
         });
         array.sort(function (a, b) {
           return setDate(a.fechas) - setDate(b.fechas);
@@ -70,11 +80,11 @@ const Projects = () => {
     projects();
     setImages([
       "https://repository-images.githubusercontent.com/341706388/1fb2ac80-7ed1-11eb-81d6-ec6ad9b24c21",
+      "https://repository-images.githubusercontent.com/342668568/62798200-7edb-11eb-9fb8-258ded4dd0bd",
+      "https://repository-images.githubusercontent.com/345266973/fbf56380-7edc-11eb-85a1-23b7ee5a996f",
       "https://repository-images.githubusercontent.com/344655480/981a6d00-7ed3-11eb-91e6-aa70c107b186",
       "https://repository-images.githubusercontent.com/312438893/addc6200-7ed4-11eb-9e02-2e2fad688bdc",
-      "https://repository-images.githubusercontent.com/342668568/62798200-7edb-11eb-9fb8-258ded4dd0bd",
       "https://repository-images.githubusercontent.com/336101675/edf31300-7edb-11eb-8183-caf5773e6b73",
-      "https://repository-images.githubusercontent.com/345266973/fbf56380-7edc-11eb-85a1-23b7ee5a996f",
       "https://repository-images.githubusercontent.com/313143497/7c699380-7ee0-11eb-8052-1221ba6fe82d",
     ]);
   }, []);
